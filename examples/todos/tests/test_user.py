5e30322d65f63975b63cd80f6bfc92118d04c21a
guyskk <guyskk@qq.com>
2016-05-12 14:09:30 +0000

    fix examples/todos

--- examples/todos/tests/test_user.py
+++ examples/todos/tests/test_user.py
@@ -1,26 +1,24 @@
 # coding:utf-8
 from __future__ import unicode_literals
-from todos import api
 
 
-def test_get(res):
-    with api.test_client() as c:
-        assert c.user.get().code == 403
+def test_get(guest, res):
+    assert guest.user.get().status_code == 403
     resp = res.user.get()
-    assert resp.code == 200
-    assert resp.rv["name"] == "tester"
-    assert resp.rv["photo"] == "http://pic.todos.com/photo/tester.png"
-    assert "pwdhash" not in resp.rv
+    assert resp.status_code == 200
+    assert resp.json["email"] == "tester@todos.com"
+    assert resp.json["photo"] == ""
+    assert "pwdhash" not in resp.json
 
 
 def test_put(res):
     resp = res.user.put({
         "name": "super_tester"
     })
-    assert resp.code == 200
-    assert resp.rv["message"] == "OK"
+    assert resp.status_code == 200
+    assert resp.json["name"] == "super_tester"
     resp = res.user.get()
-    assert resp.code == 200
-    assert resp.rv["name"] == "super_tester"
-    assert resp.rv["photo"] == "http://pic.todos.com/photo/tester.png"
-    assert "pwdhash" not in resp.rv
+    assert resp.status_code == 200
+    assert resp.json["name"] == "super_tester"
+    assert resp.json["photo"] == ""
+    assert "pwdhash" not in resp.json
