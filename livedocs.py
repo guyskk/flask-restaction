65cedc4a9bd355c277efe61d9c0f4eb17f08241d
guyskk <guyskk@qq.com>
2016-10-28 15:07:15 +0000

    remove livedocs.py

--- livedocs.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from livereload import Server, shell
-
-
-server = Server()
-
-server.watch('docs/source/*.rst', shell('make html', cwd='docs'))
-server.serve(root='docs/build/html/')
